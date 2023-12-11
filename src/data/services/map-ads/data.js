c4f0680c9809bf4452149c78e67992a70e00a8b0
Anirudh Saini <anirudh.saini852@gmail.com>
2023-12-11 12:15:52 +0530

    add content

--- src/data/services/map-ads/data.js
+++ src/data/services/map-ads/data.js
@@ -1,23 +1,19 @@
 export const main = {
-	heading: "",
-	text: "",
+	heading: "The Role Map Ads Play in Marketing For Small Businesses",
+	text: "We find Map Ads (Performance Max campaigns for store goals) work best for businesses like restaurants, gas stations, tire shops, hotels, tourist attractions, urgent cares, and retail shops; businesses people might be searching for while in their car using Google maps to find where to go. Below we will go over the basics of Map Ads, a comparison of proper versus improper execution of Map Ads, cover best practices, and we will close out with a step-by-step guide as to how you can implement Map Ads for your business.",
 };
 
 export const guide = [
 	{
-		heading: "",
-		text: "",
-	},
-	{
-		heading: "",
-		text: "",
-	},
+		heading: "What are Map Ads?",
+		text: "Map Ads are a way to appear recommended above businesses like yours when someone uses Google Maps to search for a product or service related to your business, even if your competition is closer to the person searching. These are setup through Google Ads Performance Max campaigns for store goals. Now, by creating normal Performance Max campaign you can set a specific conversion goal for bringing foot traffic into your business. And what’s something people out and about are always using? An app on your phone for navigation. Google obviously knows this too, so when you set store visits as a campaign goal Google puts you in front of people searching on Google Maps or even Waze (Waze will be coming to Google Ads sometime next year, no confirmed release date is public).",
+	}
 ];
 
 export const comparison = {
-	heading: "",
-	correct: "",
-	wrong: "",
+	heading: "Good Map Ads execution vs bad Map Ads execution",
+	correct: "A restaurant in Boston wants to attract more tourists who are driving in from Connecticut. They setup Google My Business with all of their accurate business information, establish location based bidding for their ads, and highlight a lunch promotion they’re running. It’s now Saturday, tourists are filling in and out asking about the promotion. ",
+	wrong: "A restaurant in Boston wants to attract more tourists who are driving in from Connecticut. They don’t add their accurate operating hours on Google My Business and don’t set any location based bidding for their ads. It’s now Saturday, the restaurants  ads aren’t reaching people close to the restaurant, and those who see the ads don’t know if the restaurant is open.",
 };
 
 export const faq = [
@@ -40,25 +36,24 @@ export const faq = [
 ];
 
 export const steps = {
-	heading: "",
-	steps: [""],
-	last: "",
+	heading: "Now that you have an idea of how map ads can add value to your small business, it’s time to start your first campaign...",
+	steps: ["Search for Google Business Profile (GBP) and create an account or sign in. Click ‘Edit Profile’, and make sure you have accurately filled out all of the information under the ‘About’, ‘Contact’, ‘Location’, and ‘Hours’ tabs. Under ‘More’ you will see more options which can be added if they’re applicable to your business","Search for Google Ads and sign in or create an account being sure to use the same email address as you used to create your GBP. Click the 'Create' button and select 'Campaign.' Choose 'Local store visit and promotions' as your campaign goal and select 'Performance Max' as the campaign type.","You'll then confirm your conversion goals, which could be store visits, contacts, or directions. Add your business location, give your campaign a name, and click ‘Continue’","Now you can set how you want to spend you ads budget in bidding preferences. In the bidding section, choose a bid strategy, either focusing on conversions or conversion value. If you want to target new customers specifically, there's an option for that too","Navigate to campaign settings, select the locations (zip, city, state, radius) you want to target as well as the languages your customers speak","At this point you’re ready to setup your asset group by giving it a name and adding your images, logos, videos, headlines, descriptions, and calls to action to the campaign. Additionally you can further optimize your campaign with audience signals but we won’t be covering that here","The next step is to set your average daily budget, being sure to look at how that translates to projected ad spend for the month. Ironically yes, you setup how you want to spend your budget before actually setting your budget ","Finally you can review your entire campaign and click ‘Publish Campaign’. From there, monitor the ads performance daily adjusting the locations targeted, daily spending, and keywords targeted as needed. At this point you may be confused as to how you will show up in Google Maps through navigational search. Well, Google looks at your business and how you setup your campaign to deliver your ad in places where it makes the most sense for them to be. For example, when you selected ‘Local store visit and promotion’ as your goal and ‘Performance Max’ as your campaign type, that combined with your Google Business Profile, tells Google exactly where your ads need to go in order to reach your target"],
+	last: "Or meet with one of our Google Certified marketing specialists and let us take care of everything for you!",
 };
 
 export const info = {
 	heading: "",
 	info: [
 		[
-			{ head: "", text: "" },
-			{ head: "", text: "" },
+			{ head: "Fully Set-up Google Business Profile", text: "This we cannot stress enough. All too often we see small businesses with missing business hours, no images, no services offered listed, improper industry category, and more. Setting up your Google Business Profile is the first step to letting Google know you exist. Without being properly setup, they won’t recommend you over competitors. Simple as that." },
+			{ head: "Target Location-Based Keywords ", text: "When you’re deciding what keywords to target for your map ads, consider the geo-location that you want your ads to reach. Then, create and target keyword phrases such as ‘restaurants near me’ or ‘best places to eat in Boston’. The goal with these location-based keywords is to utilize phrases that customers typically search for when looking to engage with a business like yours." },
 		],
 		[
-			{ head: "", text: "" },
-			{ head: "", text: "" },
+			{ head: "Geo-Location Targeting", text: "Related to location-based keywords, geo-location targeting is your ability to select a specific area to target with your map ads. You can target anything from specific zip codes, cities, states, regions, or utilize the basic radius targeting where you can reach any area within a specific radius of your business. Geo-location targeting is best utilized by first determining where your potential customers live, then by analyzing your competitors presence in the area, and finally assessing how relevant your ads would be to the targeted locations given seasonal changes. " },
+			{ head: "Ask For Reviews and Respond to Everyone", text: "Getting reviews from your customers is important for 2 main reasons. First, this is a simple way to gain creditability. Second, it shows Google you’re business is better liked in comparison to your competitors. The best way to get reviews is simply by asking your customers to leave a review to help your business grow. But getting reviews is only half of it. Responding to these reviews is important too. For Google, responding to reviews tells them your business is actively engaging with customers. For your customers, this is a chance for them to see both sides of a review. A poor review can hurt a business, but if you respond to that poor review in a positive manner, that response can negate the negative impact of that review in the eyes of the customer. " },
 		],
 		[
-			{ head: "", text: "" },
-			{ head: "", text: "" },
+			{ head: "Monitor your Map Ads and Make Changes as Needed", text: "While running your map ads campaigns, you will notice the reach you are getting over time. In Google Ads Manager, you can look at a report of how your ads are performing daily, weekly, or monthly. Let’s say you start off running map ads every day of the week 24/7. You see people are interacting with your ad during the week days, but that interaction is not turning into new business. But, on weekends you see that your map ads are getting lots of reach, and in turn you are getting lots of new business on the weekends. A change you might want to make your campaign then would be to only set your ads to run over the weekend. This saves you money on ad spend, while not missing out on the weekend business. Now this is a very basic example, but should give you the right idea. " },
 		],
 	],
 };
--- src/data/services/meta-ads/data.js
+++ src/data/services/meta-ads/data.js
@@ -1,23 +1,19 @@
 export const main = {
-	heading: "",
-	text: "",
+	heading: "The Role Meta Ads Play in Marketing For Small Businesses",
+	text: "Meta Ads, formerly Facebook Ads, now allows for you to run ads on both Meta and Instagram. We find that Meta Ads take a bit more effort to get right when compared to Google Ads, but still can be valuable when used by businesses to target people who have already viewed their website. Technically you can optimize that grouping further, but to not over complicate things we’ll leave it at that generalization. Yes, there’s billions of people using Facebook and Instagram daily. But it’s important to realize that Meta Ads are best used to re-engage with people who have already shown interest in your products or services in the past. It's not just about reaching a vast audience; it's about reaching the right people with the right message at the right time. Below we'll dive into what Meta Ads are, the difference between well-executed and poorly executed Meta Ads campaigns, Meta Ads best practices, and a step-by-step guide to get you started running your first campaign.",
 };
 
 export const guide = [
 	{
-		heading: "",
-		text: "",
-	},
-	{
-		heading: "",
-		text: "",
-	},
+		heading: "What are Meta Ads?",
+		text: "Meta Ads enable businesses to target specific audiences, making it best used for what is called “retargeting” (the process of identifying potential customers who have shown interest in your business, then targeting them with ads in an attempt to drive them to a conversion). This is different from your more general Google Ads which are better for first interactions with new business. Meta Ads are for that second interaction which you hope leads to the prospect visiting your website, booking a meeting, or giving you a phone call. As a result, when you run Meta Ads you should expect to see higher engagement rates leading to better conversion on your campaigns if executed correctly.  ",
+	}
 ];
 
 export const comparison = {
-	heading: "",
-	correct: "",
-	wrong: "",
+	heading: "Well executed Meta Ads vs poorly executed Meta Ads",
+	correct: "A tanning salon targets previous customers with a summer promotion using a custom audience created from their CRM email list. The campaign features clear images and content, leading to a surge in customers taking advantage of the promo.",
+	wrong: "A tanning salon targets a broad demographic on Meta, using outdated images and hastily written content. This leads to minimal business impact as the ads fail to connect with interested customers.",
 };
 
 export const faq = [
@@ -40,25 +36,24 @@ export const faq = [
 ];
 
 export const steps = {
-	heading: "",
-	steps: [""],
-	last: "",
+	heading: "Now that you have an idea of how Meta ads can add value to your small business, it’s time to start your first campaign...",
+	steps: ["Visit Facebook and create a page for you r business if you don’t already have one. Then, search for Meta Ads Manager and sign in with your Facebook account. Go through the setup flow, add your payment details, and save","Once logged in successfully, you’ll see your dashboard which should be empty at this point. To create your first campaign, you can click the ‘Create’ button which can be found in the top left of the dashboard","Next you’ll be asked to choose from 11 objective your would like your campaign to focus on. We recommend you select either ‘store traffic’, ‘website traffic’, or ‘lead generation’ as yours","After establishing an objective, you’ll be asked to set an audience to target your ads towards. For your first campaign we recommend you export the contact information of your past customers from your CRM and upload them as a custom audience. Then you can create a lookalike audience from that reaches people who are similar to the customers you uploaded","Now you’ll be asked to set your budget using one of two options, daily budget or lifetime budget. For your first campaign  let’s go with daily budget. Additionally you can set your bidding strategy to goal-based bidding which will automate the bidding process so you can just let your campaign run","Create the visual elements of your ad based on what your campaign objective is. Be sure to take note at Meta design standards to make sure your images fit properly and that your text content is properly formatted. Preview your ad, and publish it live when you’re satisfied","While your ad is running continuously monitor the ads performance. With your first ad campaign there is a bit of a learning curve. Don’t be afraid of testing and trying new ideas in an attempt to reach your customers"],
+	last: "Or meet with one of our Google Certified marketing specialists and let us take care of everything for you!",
 };
 
 export const info = {
-	heading: "",
+	heading: "Meta Ads Best Practices for Small Businesses",
 	info: [
 		[
-			{ head: "", text: "" },
-			{ head: "", text: "" },
+			{ head: "Experiment to See What Works", text: "Meta ads are know to be the more complex of paid advertising platforms. Sometimes you’ll see your competitors running an ad with success, then you try running a similar campaign just to get zero reach to potential customers. Don’t expect your first campaign to work out, be patient and continue to test new changes. " },
+			{ head: "Experiment to See What Works", text: "As a locally owned small business you should always make an effort to utilize real photos of your business in action. Whether this is getting images of employees working or images of products, the images you take are how you are able to visually communicate your business to people. This goes for everything, but Meta ads especially. Since the goal with Meta ads is “retargeting” you need to include imagery connected to what product or service the customer showed interest in. The customer should see the image and think of your product or service before reading the content of the ad.  " },
 		],
 		[
-			{ head: "", text: "" },
-			{ head: "", text: "" },
+			{ head: "Understand Your Customers Needs", text: "This is key to any successful paid ads campaign, but even more so for Meta ads. Take your time with the research, don’t rush it. Set up your campaign promoting the wrong thing or targeting the wrong audience and it will kill your chances of success. Set up your campaign correctly and you will wonder why everyone makes Meta Ads out to be so complicated." },
+			{ head: "Utilize Custom Audiences", text: "Custom audiences enable you to target people who have interacted with your business, whether that be someone visiting your website or through social media. In order to allow Meta to identify people that have shown interest in your business by visiting your website, you need to install something called Meta Pixels onto your site. This enables Meta to track who visits your website so that you can target with them Meta ads later on. " },
 		],
 		[
-			{ head: "", text: "" },
-			{ head: "", text: "" },
+			{ head: "Be Thorough and Accurate", text: "At the end of the day Meta has a process they use to determine which ads to show to who and when. That why when you’re creating a Meta ads campaign you must be careful to be clear with your business goals and objectives. Don’t copy someone else’s ad, be creative and make your own. If you are thorough and accurate, Meta will do the work needed to get your ads in front of the right people based on the guidelines you give. " },
 		],
 	],
 };
