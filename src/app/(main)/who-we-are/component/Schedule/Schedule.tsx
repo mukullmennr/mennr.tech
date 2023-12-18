a675d77844c1d4511d3d09dae92a545e8efc44cc
rohan <rohanverma031@gmail.com>
2023-12-18 16:34:42 +0530

    chipy who-we-help

--- src/app/(main)/who-we-are/component/Schedule/Schedule.tsx
+++ src/app/(main)/who-we-are/component/Schedule/Schedule.tsx
@@ -10,11 +10,18 @@ export default function Schedule() {
 					<img src="/home/whoweare/get-to-know-us.svg" />
 				</div>
 
-				<div className="calender-container">
-					<iframe
-						src="https://meetings.hubspot.com/rohan90?embed=true"
-						frameBorder="0"
-					></iframe>
+				<div className="chipy-parent">
+					<div className="calender-container" id="calender">
+						<iframe
+							src="https://meetings.hubspot.com/rohan90?embed=true"
+							frameBorder="0"
+						></iframe>
+					</div>
+
+					<img
+						src="/common/chipy/chipy-call-color.svg"
+						alt="chipy contact"
+					/>
 				</div>
 			</div>
 		</div>
