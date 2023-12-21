266efdeddc57e9689feaef85db2369da931a72fd
rohan <rohanverma031@gmail.com>
2023-12-21 18:34:19 +0530

    linting errors fixed

--- src/app/(main)/component/info/InfoOne.tsx
+++ src/app/(main)/component/info/InfoOne.tsx
@@ -5,23 +5,32 @@ export default function InfoOne() {
 		<div className="info-item info-one">
 			<div className="container">
 				<p>
-				If it wasn’t already clear, we add value to local businesses across America like yours by making sure your customers can find you online. Here’s how we do it.
+					If it wasn’t already clear, we add value to local businesses
+					across America like yours by making sure your customers can
+					find you online. Here’s how we do it.
 				</p>
 				<p>
 					<span>
-					We start with an initial consultation where our marketing strategist learns about your business, including location, customer base, online presence, needs, and goals.
+						We start with an initial consultation where our
+						marketing strategist learns about your business,
+						including location, customer base, online presence,
+						needs, and goals.
 					</span>
 					<img src="/common/chipy/chipy-meet.svg" alt="chipy" />
 				</p>
 				<p>
 					<img src="/common/chipy/chipy-call-color.svg" alt="chipy" />
 					<span>
-					Based on your initial consultation, our strategist creates a detailed 6-month marketing plan, personalized to your business's needs and goals.
+						Based on your initial consultation, our strategist
+						creates a detailed 6-month marketing plan, personalized
+						to your business&apos;s needs and goals.
 					</span>
 				</p>
 				<p>
 					<span>
-					Once we complete your 6-month plan, your strategist meets with you, virtually or in-person, to walk you through our proposed plan.
+						Once we complete your 6-month plan, your strategist
+						meets with you, virtually or in-person, to walk you
+						through our proposed plan.
 					</span>
 
 					<img
