43686d81f59710c1537489ab3a1a655cadf29350
rohan <rohanverma031@gmail.com>
2024-01-16 16:13:13 +0530

    faq redesign

--- src/app/(main)/solutions/page.tsx
+++ src/app/(main)/solutions/page.tsx
@@ -9,13 +9,13 @@ import Plans from "./component/plans/Plans";
 export default function page() {
 	return (
 		<>
-			<Main />
-			<Plans data={plans} />
-			<Steps
+			{/* <Main /> */}
+			{/* <Plans data={plans} /> */}
+			{/* <Steps
 				data={steps}
 				isServices={false}
 				classname="solutions-steps"
-			/>
+			/> */}
 			<FAQ ques={faq.data}>{faq.heading}</FAQ>
 		</>
 	);
