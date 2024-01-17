081e66494e4fa3299c0e9f648164cb41b102273f
rohan <rohanverma031@gmail.com>
2024-01-17 12:19:25 +0530

    not-sure component completed

--- src/app/(main)/solutions/page.tsx
+++ src/app/(main)/solutions/page.tsx
@@ -1,25 +1,15 @@
 import React from "react";
 
-import { faq, steps, plans } from "@/data/solution";
-import Main from "./component/main/Main";
+import { faq, notSure } from "@/data/solution";
 import FAQ from "@/components/FAQ/FAQ";
-import Steps from "@/components/Steps/Steps";
-import Plans from "./component/plans/Plans";
 import NotSure from "./component/not-sure/NotSure";
 
 export default function page() {
 	return (
 		<>
-			{/* <Main /> */}
-			{/* <Plans data={plans} /> */}
-			{/* <Steps
-				data={steps}
-				isServices={false}
-				classname="solutions-steps"
-			/> */}
 			<FAQ ques={faq.data}>{faq.heading}</FAQ>
 
-			<NotSure />
+			<NotSure data={notSure} />
 		</>
 	);
 }
