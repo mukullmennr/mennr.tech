3dfb58bc9ba5fc54317461c7da7dcb79a2272184
rohan <80212018+rohan031@users.noreply.github.com>
2024-02-01 12:24:17 +0530

    Dev (#26)
    * added docker files
    
    * removed toke from github
    
    ---------
    
    Co-authored-by: cyclic-sh[bot] <84505167+cyclic-sh[bot]@users.noreply.github.com>

--- src/server-actions/actions.js
+++ src/server-actions/actions.js
@@ -11,7 +11,7 @@ export const addMail = async (email) => {
 		});
 		const headers = {
 			"Content-Type": "application/json",
-			Authorization: `Bearer ${process.env.HUBSPOT}`,
+			Authorization: `Bearer ${process.env.NEXT_PUBLIC_HUBSPOT}`,
 		};
 
 		let res = await fetch(url, {
@@ -23,12 +23,12 @@ export const addMail = async (email) => {
 		res = await res.json();
 
 		if (res.status === "error") {
-			if (res.category === "CONFLICT")
-				throw new Error("Email already exists");
-			else if (res.category === "VALIDATION_ERROR") {
-				throw new Error("Invalid email");
+			if (res.category === "CONFLICT") {
+				throw new Error("1");
+			} else if (res.category === "VALIDATION_ERROR") {
+				throw new Error("2");
 			} else {
-				throw new Error("Can't add email right now");
+				throw new Error("3");
 			}
 		}
 
@@ -37,9 +37,6 @@ export const addMail = async (email) => {
 			message: "Successfully added email",
 		};
 	} catch (error) {
-		return {
-			success: false,
-			message: error.message,
-		};
+		throw new Error(error.message);
 	}
 };
