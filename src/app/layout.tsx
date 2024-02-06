36d596b6786ee5b6692b775115dbab1e62d1747d
rohan <rohanverma031@gmail.com>
2024-02-06 13:02:54 +0530

    next js third parties

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "../styles/main.scss";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -25,24 +26,25 @@ export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html lang="en">
 			<head>
-				<Script id="gtag-code">
+				{/* <Script id="gtag-code">
 					{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');`}
-				</Script>
+				</Script> */}
+				<GoogleTagManager gtmId={`${process.env.NEXT_PUBLIC_GTM}`} />
 			</head>
 
 			<body className={plusJakartaSans.className}>
-				<noscript>
+				{/* <noscript>
 					<iframe
 						src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM}`}
 						height="0"
 						width="0"
 						style={{ display: "none", visibility: "hidden" }}
 					></iframe>
-				</noscript>
+				</noscript> */}
 
 				{children}
 			</body>
