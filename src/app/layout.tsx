fe5b86c22396c2c022bf763ef51fec9036d7df19
rohan <rohanverma031@gmail.com>
2024-02-06 11:52:33 +0530

    added env for gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,14 +30,14 @@ export default function RootLayout({ children }: RootLayoutProps) {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N9WNGNN2');`}
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');`}
 				</Script>
 			</head>
 
 			<body className={plusJakartaSans.className}>
 				<noscript>
 					<iframe
-						src="https://www.googletagmanager.com/ns.html?id=GTM-N9WNGNN2"
+						src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM}`}
 						height="0"
 						width="0"
 						style={{ display: "none", visibility: "hidden" }}
